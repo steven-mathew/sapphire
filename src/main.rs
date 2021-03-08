6d87b453fa566dd665fc1df4a33f5bd1132d42f1
steven-mathew <steven.mathew@mail.utoronto.ca>
2021-03-07 23:33:57 -0500

    Created a database struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
+use std::{collections::HashMap, fs::read_to_string};
+
 fn main() {
     let mut arguments = std::env::args().skip(1);
-    let key = arguments.next().unwrap();
-    let value = arguments.next().unwrap();
-    println!("The key is {}", key);
-    println!("The value is {}", value);
+    let key = arguments.next().expect("Key was not there");
+    let value = arguments.next().expect("Value was not there");
+    println!("The key is {} and value is {}", key, value);
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents).unwrap();
+
+    let database = Database::new().expect("Database::new() crashed");
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        // Read the kv.db file
+        // let contents = match std::fs::read_to_string("kv.db") {
+        //     Ok(c) => c,
+        //     Err(error) => {
+        //         return Err(error);
+        //     }
+        // };
+       
+        // Morally equivalent to...
+        let mut map = HashMap::new();
+        let contents = std::fs::read_to_string("kv.db")?;
+        for line in contents.lines() {
+            let mut chunks = line.splitn(2, '\t');
+            let key = chunks.next().expect("No key!");
+            let value = chunks.next().expect("No value!");
+            map.insert(key, value);
+        }
+        // Parse the stirng
+         
+        // Populate the map
+        Ok(Database {
+            map: map
+        })
+    }
 }
